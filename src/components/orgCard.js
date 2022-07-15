82106d1108092ff64028f546fa3a6fd4021964e4
kasun <kasundularaam@gmail.com>
2022-07-15 13:40:29 +0530

    flex styles added

---
+++ src/components/orgCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const orgCard = () => {
+  return (
+    <div className="orgCard">
+      <img src="https://i.imgur.com/jPXJiFj.png" alt="" className="round_img" />
+      <div>Google LLC</div>
+    </div>
+  );
+};
+
+export default orgCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
+import OrgCard from "../components/orgCard";
+
 const Home = ({ user }) => {
   return (
     <section className="section">
       <h1>Home Page</h1>
       <h4>Hello, {user?.name}</h4>
+      <div className="Row">
+        <OrgCard />
+        <OrgCard />
+        <OrgCard />
+        <OrgCard />
+      </div>
     </section>
   );
 };
